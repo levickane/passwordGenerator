a0ed15ef4881b194297fa24b627bb01ce64f1bc2
Levi Kane <oneleggedlevi09@gmail.com>
2021-04-01 09:39:59 -0500

    deleted last console.log

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ var generateBtn = document.querySelector('#generate')
 
 // Write password to the #password input
 function writePassword() {
-  console.log('hello')
   var password = generatePassword()
   var passwordText = document.querySelector('#password')
 
